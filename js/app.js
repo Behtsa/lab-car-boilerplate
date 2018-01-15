90047982dde75f45b4883c0511f2ef9021a1efa7
Behtsa <monze_gdbm@hotmail.com>
2018-01-15 10:16:47 -0600

    api google map

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-// La funcionalidad de tu proyecto
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(19.432608, -99.133208),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
